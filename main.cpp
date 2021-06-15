2cbacd6ea8088a188357c4e327f119723b1eb671
vskavronsky <vskavronsky92@gmail.com>
2021-06-15 22:21:15 +0300

    [develop] Added sum realization of the two numbers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
 int main() {
+    int x, y;
     
-        
+    std::cout << "Enter first number: ";
+    std::cin >> x;
+    
+    std::cout << "Enter second number: ";
+    std::cin >> y;
+    
+    std::cout << "Sum of the two numbers: " << x + y << std::endl;
+    
     return 0;
 }
